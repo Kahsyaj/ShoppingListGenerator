784e4d64131790f5507a08c05e72153ea63b03b0
Jérémy Coley <jcoley@digitaleo.com>
2018-06-04 17:30:46 +0200

    db_init created, some improvements on attributes

--- Manager.py
+++ Manager.py
@@ -15,10 +15,10 @@ class Manager:
     def __init__(self, mode="file", usr="root", psswd="root"):
         self.mode = mode
         if mode == "file":
-            self.dest = "../ShpLstSave/"
+            self.folder = "../ShpLstSave/"
             self.ext = ".bak"
         elif mode == "database":
-            self.dest = "ShoppingList"
+            self.database = "ShoppingListGenerator"
             self.user = usr
             self.password = psswd
             self.tables = []
@@ -26,23 +26,41 @@ class Manager:
             raise ValueError('You can only choose between "file" and "database".')
 
     # Getters and setters
-    def get_dest(self):
-        return self.dest
+    def get_folder(self):
+        return self.folder
 
-    def get_tbls(self):
-        return self.tbls
+    def get_database(self):
+        return self.database
+
+    def get_tables(self):
+        return self.tables
 
     def get_ext(self):
         return self.ext
 
-    def set_dest(self, new):
-        self.dest = new
+    def get_user(self):
+        return self.user
 
-    def set_tbls(self, new):
-        self.tbls = new
+    def get_password(self):
+        return self.password
+
+    def set_folder(self, new):
+        self.folder = new
+
+    def set_database(self, new):
+        self.database = new
+
+    def set_tables(self, new):
+        self.tables = new
 
     def set_ext(self, new):
         self.ext = new
+
+    def set_user(self, new):
+        self.user = new
+
+    def set_password(self, new):
+        self.password = new
 
     """
     From an object, generate a file name (based on the type of the object and it's name) for saving / managing
@@ -50,6 +68,13 @@ class Manager:
     """
     def gen_file_name_from_obj(self, obj):
         return re.sub(r"'(?P<name>[\w]*)'", r"\g<name>" + "_{}{}".format(obj.get_name(), self.ext), type(obj))
+
+    """
+    From an object, generate the associated table name
+    :param obj : the object from which generate the name
+    """
+    def gen_table_name_from_obj(self, obj):
+        return re.sub(r"'(?P<name>[\w]*)'", r"\g<name>", type(obj))
 
     """
     Save an object (Serialize) into a file (name given with gen_file_name_from_obj()
@@ -65,7 +90,7 @@ class Manager:
     :return : the object loaded
     """
     def file_load(self, file_name):
-        with open(self.dest + file_name, "rb") as f:
+        with open(self.folder + file_name, "rb") as f:
             return pickle.load(f)
 
     """
@@ -74,8 +99,8 @@ class Manager:
     """
     def files_load(self):
         objs = dict()
-        for file in os.listdir(self.dest):
-            with open(self.dest + file, "rb") as f:
+        for file in os.listdir(self.folder):
+            with open(self.folder + file, "rb") as f:
                 obj = pickle.load(f)
                 objs[file] = obj
         return objs
@@ -97,3 +122,20 @@ class Manager:
             "deleted INT(1) DEFAULT 0, CONSTRAINT Recipe_PK PRIMARY KEY (id_meal,id_ingredient), "
             "CONSTRAINT Recipe_Meal_FK FOREIGN KEY (id_meal) REFERENCES Meal(id_meal), "
             "CONSTRAINT Recipe_Ingredient0_FK FOREIGN KEY (id_ingredient) REFERENCES Ingredient(id_ingredient)) ENGINE=InnoDB")
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS ShoppingList (id_shoppinglist INT NOT NULL AUTO_INCREMENT, date_shoppinglist DATE, "
+            "quantity INT, deleted INT(1) DEFAULT 0, CONSTRAINT ShoppingList_PK PRIMARY KEY (id_shoppinglist)) ENGINE=InnoDB"
+        )
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS Purchase ( id_ingredient INT NOT NULL, id_shoppinglist Int NOT NULL, "
+            "quantity INT NOT NULL, CONSTRAINT Purchase_PK PRIMARY KEY (id_ingredient,id_shoppinglist), "
+            "CONSTRAINT Purchase_Ingredient_FK FOREIGN KEY (id_ingredient) REFERENCES Ingredient(id_ingredient), "
+            "CONSTRAINT Purchase_ShoppingList0_FK FOREIGN KEY (id_shoppinglist) REFERENCES ShoppingList(id_shoppinglist)) ENGINE=InnoDB"
+        )
+
+    """
+    """
+    def db_save(self, obj):
+        mariadb_connect = mariadb.connect(user=self.user, password=self.password, database=self.dest)
+        cursor = mariadb_connect.cursor(Prepared=True)
+        cursor.execute
