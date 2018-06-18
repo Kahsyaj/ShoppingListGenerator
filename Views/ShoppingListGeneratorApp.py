5f46aca936032e8ff5b303c5425d85e5c45d364a
Jérémy Coley <jcoley@digitaleo.com>
2018-06-18 14:01:37 +0200

    Created different layout classes (to be continued).

--- Views/ShoppingListGeneratorApp.py
+++ Views/ShoppingListGeneratorApp.py
@@ -1,10 +1,63 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
+from kivy.uix.listview import ListItemButton
 
-class MainMenu(GridLayout):
+
+class MainButtonList(ListItemButton):
     pass
 
+
+class MainMenuLayout(GridLayout):
+    pass
+
+
+class CreateIngredientLayout(GridLayout):
+    pass
+
+
+class DelIngredientLayout(GridLayout):
+    pass
+
+
+class SetIngredientLayout(GridLayout):
+    pass
+
+
+class DisplayIngredientLayout(GridLayout):
+    pass
+
+
+class CreateMealLayout(GridLayout):
+    pass
+
+
+class DelMealLayout(GridLayout):
+    pass
+
+
+class SetMealLayout(GridLayout):
+    pass
+
+
+class DisplayMealLayout(GridLayout):
+    pass
+
+
+class CreateShoppingListLayout(GridLayout):
+    pass
+
+
+class DelShoppingListLayout(GridLayout):
+    pass
+
+
+class SetShoppingListLayout(GridLayout):
+    pass
+
+
+class DisplayShoppingListLayout(GridLayout):
+    pass
 
 
 class ShoppingListGeneratorApp(App):
